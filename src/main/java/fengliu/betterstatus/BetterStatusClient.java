21effeaeec6bc7d2d0196587606a37cfbdf6d31f
FengLiuFeseliud <1718089268@qq.com>
2023-01-30 02:41:03 +0800

    Init Mod

---
+++ src/main/java/fengliu/betterstatus/BetterStatusClient.java
@@ -0,0 +1,16 @@
+package fengliu.betterstatus;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BetterStatusClient implements ModInitializer {
+	public static final String MOD_ID = "betterstatus";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
